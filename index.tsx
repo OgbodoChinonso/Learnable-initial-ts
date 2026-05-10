7ffb107f4292fb239b3b066b464a4c30ad615092
ogbodo Paschal <ogbodopaschal@gmail.com>
2026-05-10 23:49:26 +0100

    update index.tsx

--- index.tsx
+++ index.tsx
@@ -14,17 +14,22 @@ interface Admin {
 
 type Person = User | Admin;
 
-export function filterPersons(persons: Person[], personType: 'user', criteria: Partial<Omit<User, 'type'>>): User[];
-export function filterPersons(persons: Person[], personType: 'admin', criteria: Partial<Omit<Admin, 'type'>>): Admin[];
+export function filterPersons(persons: Person[], personType: 'user', criteria: Partial<User>): User[];
+export function filterPersons(persons: Person[], personType: 'admin', criteria: Partial<Admin>): Admin[];
 
 export function filterPersons(persons: Person[], personType: string, criteria: any): Person[] {
     return persons
         .filter((person) => person.type === personType)
         .filter((person) => {
-            let criteriaKeys = Object.keys(criteria) as (keyof typeof criteria)[];
-            return criteriaKeys.every((childKey) => {
-                return person[childKey as keyof Person] === criteria[childKey];
+            let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
+            return criteriaKeys.every((fieldName) => {
+                return person[fieldName] === criteria[fieldName];
             });
         });
 }
+
+
+
+
+
 
